837c4a532451eca5a945d0b8f63622ed22137bbe
WHYSOSERIOUS1234 <103112614+WHYSOSERIOUS1234@users.noreply.github.com>
2022-09-01 20:45:21 -0700

    "feature2"

--- index.js
+++ index.js
@@ -1,5 +1,63 @@
-(function() {
+(function () {
   'use strict';
+//access stoplight element
+//access slowlight element
+//access golight element
+let stoplight = document.getElementById('stopLight')
+let slowlight = document.getElementById('slowLight')
+let golight = document.getElementById('goLight')
+//acess stopButton 
+//access slowButton 
+//access goButton
+let stopButton = document.getElementById('stopButton')
+let slowButton = document.getElementById('slowButton')
+let goButton = document.getElementById('goButton')
 
-  // YOUR CODE HERE
-})();
+
+
+
+
+
+
+
+
+stopButton.addEventListener('click', (e) => {
+  if(stoplight.style.backgroundColor === 'red') {
+    stoplight.style.backgroundColor = 'black'
+  } else {
+    stoplight.style.backgroundColor = 'red'
+  }
+})
+
+
+
+slowButton.addEventListener('click', () => {
+  if(slowlight.style.backgroundColor === 'yellow') {
+    slowlight.style.backgroundColor = 'black'
+  } else {
+    slowlight.style.backgroundColor = 'yellow'
+  }})
+
+  goButton.addEventListener('click', () => {
+    if(golight.style.backgroundColor === 'green') {
+      golight.style.backgroundColor = 'black'
+    } else {
+      golight.style.backgroundColor = 'green'
+    }})
+
+    stopButton.addEventListener('mouseover',stop, false) 
+    function stop(e) {
+      console.log(stopButton.textContent)
+    }
+
+    slowButton.addEventListener('mouseover',slow, false) 
+    function slow(e) {
+      console.log(slowButton.textContent)
+    }
+
+    goButton.addEventListener('mouseover',go, false) 
+    function go(e) {
+      console.log(goButton.textContent)
+    }
+
+  })()
